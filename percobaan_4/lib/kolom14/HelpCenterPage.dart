3d09ee6f233324c1cee5774a410a9831d8613038
“Muhamad <muhamfirdaus2003@gmail.com>
2023-05-21 23:22:45 +0700

    Create HelpCenterPage.dart

---
+++ percobaan_4/lib/kolom14/HelpCenterPage.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Help Center',
+      home: HelpCenterPage(),
+      debugShowCheckedModeBanner: false, // Remove the debug banner
+    );
+  }
+}
+
+class HelpCenterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(45, 41, 96, 1),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 16.0),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      // Handle back button tap
+                    },
+                    icon: Icon(Icons.arrow_back),
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 8.0),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Bantuan',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),
+              HelpButton(
+                icon: Icons.account_circle,
+                text: 'FAQ',
+                onTap: () {
+                  // Handle FAQ button tap
+                },
+              ),
+              SizedBox(height: 16.0),
+              HelpButton(
+                icon: Icons.payment,
+                text: 'Call Center',
+                onTap: () {
+                  // Handle Call Center button tap
+                },
+              ),
+              SizedBox(height: 16.0),
+              HelpButton(
+                icon: Icons.security,
+                text: 'Resiko Pemberian Pinjaman',
+                onTap: () {
+                  // Handle Resiko Pemberian Pinjaman button tap
+                },
+              ),
+              SizedBox(height: 16.0),
+              HelpButton(
+                icon: Icons.language,
+                text: 'Draft Perjanjian',
+                onTap: () {
+                  // Handle Draft Perjanjian button tap
+                },
+              ),
+              SizedBox(height: 16.0),
+              HelpButton(
+                icon: Icons.settings,
+                text: 'Informasi Penilaian Risiko',
+                onTap: () {
+                  // Handle Informasi Penilaian Risiko button tap
+                },
+              ),
+              SizedBox(height: 16.0),
+              HelpButton(
+                icon: Icons.help,
+                text: 'Kebijakan Privasi',
+                onTap: () {
+                  // Handle Kebijakan Privasi button tap
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HelpButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final VoidCallback onTap;
+
+  const HelpButton({
+    required this.icon,
+    required this.text,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 148, 226, 191),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: ListTile(
+        leading: Icon(
+          icon,
+          color: Colors.black,
+        ),
+        title: Text(
+          text,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        trailing: Icon(
+          Icons.arrow_forward_ios,
+          color: Colors.black,
+        ),
+        onTap: onTap,
+      ),
+    );
+  }
+}
