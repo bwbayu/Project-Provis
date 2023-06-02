6fc747062b1e920e7e37d80630e0d62ea95bf743
ArdynRF <ardynstar@yahoo.com>
2023-06-02 21:45:16 +0700

    carousel_rekomen

--- percobaan_4/lib/kolom6/InvestorPage.dart
+++ percobaan_4/lib/kolom6/InvestorPage.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
+
 class InvestorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
