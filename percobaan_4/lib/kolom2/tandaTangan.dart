d4e9918ec93ceef0a00c50b7aa632ff7dc61e816
“Muhamad <muhamfirdaus2003@gmail.com>
2023-06-03 18:06:58 +0700

    Update tandaTangan.dart

--- percobaan_4/lib/kolom2/tandaTangan.dart
+++ percobaan_4/lib/kolom2/tandaTangan.dart
@@ -1,140 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class formTTD extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF2D2960),
-        appBar: AppBar(
-          backgroundColor: Color(0xFF2E2960),
-          automaticallyImplyLeading: true,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+      backgroundColor: Colors.purple[200]!,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: SvgPicture.asset(
+            'asset/images/vector.svg',
+            width: 30,
+            height: 30,
           ),
         ),
-        body: SafeArea(
-          top: true,
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(30, 10, 10, 0),
-                child: Container(
-                  width: double.infinity,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Color(0xFF2D2960),
-                  ),
-                  child: ListTile(
-                    title: Text(
-                      'Tanda Tangan',
-                      style: TextStyle(
-                        fontFamily: 'Outfit',
-                        color: Color(0xFFF7F8F9),
-                        fontSize: 25,
-                      ),
+        title: Text(''),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        top: true,
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(30, 10, 10, 0),
+              child: Container(
+                width: double.infinity,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Color(0xFF2D2960),
+                ),
+                child: ListTile(
+                  title: Text(
+                    'Tanda Tangan',
+                    style: TextStyle(
+                      fontFamily: 'Outfit',
+                      color: Color(0xFFF7F8F9),
+                      fontSize: 25,
                     ),
-                    subtitle: Text(
-                      'Tanda tangan sesuai dengan KTP di atas kertas.',
-                      style: TextStyle(
-                        fontFamily: 'Readex Pro',
-                        color: Color(0xFFF2F5F8),
-                        fontWeight: FontWeight.w300,
-                      ),
+                  ),
+                  subtitle: Text(
+                    'Tanda tangan sesuai dengan KTP di atas kertas.',
+                    style: TextStyle(
+                      fontFamily: 'Readex Pro',
+                      color: Color(0xFFF2F5F8),
+                      fontWeight: FontWeight.w300,
                     ),
                   ),
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
-                child: Container(
-                  width: double.infinity,
-                  height: 400,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.network(
-                          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-                          width: double.infinity,
-                          height: 320,
-                          fit: BoxFit.cover,
-                        ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+              child: Container(
+                width: double.infinity,
+                height: 400,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+                        width: double.infinity,
+                        height: 320,
+                        fit: BoxFit.cover,
                       ),
-                      Column(
-                        mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Text(
-                              'Upload',
-                              style: TextStyle(
-                                fontFamily: 'Readex Pro',
-                                color: Colors.white,
-                              ),
-                            ),
-                            style: ElevatedButton.styleFrom(
-                              primary: Color(0xffcb5f18),
-                              elevation: 3,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              padding: EdgeInsets.all(0),
+                    ),
+                    Column(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Upload',
+                            style: TextStyle(
+                              fontFamily: 'Readex Pro',
+                              color: Colors.white,
                             ),
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Color(0xFF2D2960),
-                  ),
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(0, 20, 10, 10),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: Text(
-                          'Lanjutkan',
-                          style: TextStyle(
-                            fontFamily: 'Readex Pro',
-                            color: Colors.white,
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xffcb5f18),
+                            elevation: 3,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: EdgeInsets.all(0),
                           ),
                         ),
-                        style: ElevatedButton.styleFrom(
-                          primary: Color(0xFF977EF2),
-                          elevation: 3,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 16,
-                            vertical: 10,
-                          ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Color(0xFF2D2960),
+                ),
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 20, 10, 10),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        'Lanjutkan',
+                        style: TextStyle(
+                          fontFamily: 'Readex Pro',
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF977EF2),
+                        elevation: 3,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 10,
                         ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
